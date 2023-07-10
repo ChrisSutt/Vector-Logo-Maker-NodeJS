b5e716b0ff3aa5b4849603dbc2c3381eba1f3e93
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-07-10 17:56:15 -0300

    fixed a couple of typos, added colorList and finished question.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const inquirer =
-const fs =
-const question =
-const file = 
-const genShape =
+const inquirer = require('inquirer');
+const fs = require('fs');
+const question = require('./lib/question');
+const file = './samples/logo.svg';
+const genShape = require('./lib/genShape');
 
+function genLogo(response) {
+    const vector = genShape(response);
+    fs.writeFile(file, vector);
+}
+
+function init() {
+    inquirer
+    .prompt(question)
+    .then((response) => {
+        genLogo(response);
+    })
+    .catch(err => {
+        console.log(err)
+    });
+}
+
+init()
