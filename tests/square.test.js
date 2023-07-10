94f6d01963f991c5d965231bbf14287fdd9a8ccd
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-07-10 20:41:34 -0300

    testing for triangle / square / circle pass

--- tests/square.test.js
+++ tests/square.test.js
@@ -1,16 +1,14 @@
-const Square = require('./Square');
+const Square = require('../lib/square');
 
 describe('Square', () => {
   test('should render SVG-Square with the provided colors and text', () => {
     const square = new Square('blue', 'SVG-Square', 'white');
-    const expectedSVG = `
-      <svg version="1.1"
-          width="300" height="200"
-          xmlns="http://www.w3.org/2000/svg">
-          <rect x="50" y="50" width="200" height="100" fill="blue" />
-          <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG-Square</text>
-      </svg>
-    `;
+    const expectedSVG = `<svg version="1.1"
+width="300" height="200"
+xmlns="http://www.w3.org/2000/svg">
+<rect x="50" y="50" width="200" height="100" fill="blue" />
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG-Square</text>
+</svg>`;
     expect(square.render()).toBe(expectedSVG.trim());
   });
 });
--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -1,16 +1,14 @@
-const Triangle = require('./Triangle');
+const Triangle = require('../lib/triangle');
 
 describe('Triangle', () => {
   test('should render SVG-Triangle with the provided colors and text', () => {
     const triangle = new Triangle('blue', 'SVG-Triangle', 'white');
-    const expectedSVG = `
-      <svg version="1.1"
-          width="300" height="200"
-          xmlns="http://www.w3.org/2000/svg">
-          <polygon points="150,20 280,180 20,180" fill="blue" />
-          <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG-Triangle</text>
-      </svg>
-    `;
+    const expectedSVG = `<svg version="1.1"
+width="300" height="200"
+xmlns="http://www.w3.org/2000/svg">
+<polygon points="150,20 280,180 20,180" fill="blue" />
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG-Triangle</text>
+</svg>`;
     expect(triangle.render()).toBe(expectedSVG.trim());
   });
 });
